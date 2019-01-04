b81f9a3924529ab48875263b1c23ff920b973d69
DESKTOP-MS94ACF\shtke <shtkeh77@gmail.com>
2019-01-05 03:46:01 +0900

    조작감의 최적의 방향을 찾는 중이다.
    - 첫번째 wasd를 모든 움직임으로 활용했음
    - 두번째 ws를 앞뒤 움직임, ad는 카메라의 회전(캐릭터도 같이)으로 했음
    
    두번째를 사용하고 보니 이런 게임에는 불편할것 같다.
    첫번째로 바꾸고 마우스로 카메라의 회전을 제어하는 것이 훨씬 나을 것 같다.

--- Win_And_Win/Assets/02.Script/PaladinCtrl.cs
+++ Win_And_Win/Assets/02.Script/PaladinCtrl.cs
@@ -21,11 +21,13 @@ public class PaladinCtrl : MonoBehaviour {
     // 캐릭터 이동 속도 변수
     public float moveSpeed = 5.0f;
     // 캐릭터 회전 속도 변수
-    //public float rotationSpeed = 100.0f;
+    public float rotationSpeed = 100.0f;
 
     // 캐릭터의 위치값을 위한 변수
     [HideInInspector] public float h = 0.0f;
     [HideInInspector] public float v = 0.0f;
+
+    private bool banjun = false;
 
     // Use this for initialization
     void Awake () {
@@ -35,22 +37,60 @@ public class PaladinCtrl : MonoBehaviour {
         tr = GetComponent<Transform>();
 
         // Animator 컴포넌트 할당
-        //animator = this.transform.GetChild(0).GetComponent<Animator>();
+        animator = this.transform.GetChild(0).GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update () {
-        h = Input.GetAxis("Horizontal");
-        v = Input.GetAxis("Vertical");
+        //h = Input.GetAxis("Horizontal");
+        //v = Input.GetAxis("Vertical");
 
-        // 전후좌우 이동 방향 벡터 계산
-        Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
+        //// 전후좌우 이동 방향 벡터 계산
+        //Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
 
-        // Translate(이동 방향 * 속도 * 변위값 * Time.deltaTime, 기준 좌표)
-        tr.Translate(moveDir.normalized * Time.deltaTime * moveSpeed, Space.Self);
+        //// Translate(이동 방향 * 속도 * 변위값 * Time.deltaTime, 기준 좌표)
+        //tr.Translate(moveDir.normalized * Time.deltaTime * moveSpeed, Space.Self);
 
-        // Vector3.up 축을 기준으로 rotSpeed만큼의 속도로 회전
-        //tr.Rotate(Vector3.up * Time.deltaTime * rotationSpeed * Input.GetAxis("Mouse X"));
+        //// Vector3.up 축을 기준으로 rotSpeed만큼의 속도로 회전
+        ////tr.Rotate(Vector3.up * Time.deltaTime * rotationSpeed * Input.GetAxis("Mouse X"));
+        //if (Input.GetKeyDown(KeyCode.S))
+        //{
+        //    banjun = true;
+        //    tr.Rotate(1.0f, 180.0f, 1.0f, 0);
+        //    //banjun = false;
+        //}
+        if (Input.GetKey(KeyCode.W))
+        {
+            tr.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+            animator.SetBool("FrontKeyDown", true);
+            animator.SetBool("BackKeyDown", false);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            tr.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+            animator.SetBool("BackKeyDown", true);
+            animator.SetBool("FrontKeyDown", false);
+        }
+        else
+        {
+            animator.SetBool("FrontKeyDown", false);
+            animator.SetBool("BackKeyDown", false);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+            tr.transform.Rotate(Vector3.up, -rotationSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.D))
+            tr.transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+        
+        if(Input.GetKey(KeyCode.Mouse0))
+        {
+            animator.SetInteger("AttackNumber", 1);
+        }
+        else
+        {
+            animator.SetInteger("AttackNumber", 0);
+        }
+
 
         // 블랜드 트리에서 v값과 h 값을 계산해서 애니메이션 실행된다.
         //animator.SetFloat("Vertical", v);
