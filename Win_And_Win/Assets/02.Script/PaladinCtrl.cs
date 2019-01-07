a94ec60bcc3e07ae101963bb49016f69c6758de0
DESKTOP-MS94ACF\shtke <shtkeh77@gmail.com>
2019-01-07 22:56:13 +0900

    여러가지 테스트 해본 결과 현재의 방법이 가장 적합한 조작법이라 생각한다.
    - wasd로 모든 방향 조작 가능(회전도 담당함)
    - 카메라는 고정된 방향으로 캐릭터를 따라 움직인다.
    - 스테이지를 위해 벽 생성

--- Win_And_Win/Assets/02.Script/PaladinCtrl.cs
+++ Win_And_Win/Assets/02.Script/PaladinCtrl.cs
@@ -4,96 +4,166 @@ using UnityEngine;
 
 public enum PlayerState
 {
-    // 추후 애니메이션에 따라 추가예정
-    idle, die, attack
+    idle, die, attackDownUp, attackUpDown, attackHorizontal, escape
 };
 
-public class PaladinCtrl : MonoBehaviour {
-
+public class PaladinCtrl : MonoBehaviour
+{
     // 캐릭터의 현재 상태 정보를 저장할 Enum 변수
     public PlayerState playerState = PlayerState.idle;
 
     // 접근해야 하는 컴포넌트는 반드시 변수에 할당한 후 사용
-    public Transform tr;
-    public Animator animator;
-    //private Rigidbody playerRigidbody;
+    private Animator animator;
+    private Rigidbody playerRigidbody;
 
     // 캐릭터 이동 속도 변수
-    public float moveSpeed = 5.0f;
+    public float moveSpeed = 3.0f;
     // 캐릭터 회전 속도 변수
     public float rotationSpeed = 100.0f;
+    // 캐릭터 이동 거리를 위한 변수
+    public Vector3 movement;
 
     // 캐릭터의 위치값을 위한 변수
     [HideInInspector] public float h = 0.0f;
     [HideInInspector] public float v = 0.0f;
 
-    private bool banjun = false;
+    // 캐릭터 이동을 제한하기 위한 변수
+    private bool CharacterControl = true;
 
-    // Use this for initialization
     void Awake () {
-        //playerRigidbody = this.GetComponent<Rigidbody>();
-
-        // 스크립트 처음에 Transform 컴포넌트 할당
-        tr = GetComponent<Transform>();
+        // Rigidbody 컴포넌트 할당
+        playerRigidbody = this.GetComponent<Rigidbody>();
 
         // Animator 컴포넌트 할당
         animator = this.transform.GetChild(0).GetComponent<Animator>();
+       
     }
 
-    // Update is called once per frame
-    void Update () {
-        //h = Input.GetAxis("Horizontal");
-        //v = Input.GetAxis("Vertical");
+    void Start()
+    {
+        // 키 입력 코루딘 시작
+        StartCoroutine("KeyDownCheckCoroutine");
+    }
 
-        //// 전후좌우 이동 방향 벡터 계산
-        //Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
+    // 이동 및 회전 값을 받아 처리하는 FixedUpdate
+    void FixedUpdate()
+    {
+        PlayerMove(h, v);
+        PlayerRotation();
+    }
 
-        //// Translate(이동 방향 * 속도 * 변위값 * Time.deltaTime, 기준 좌표)
-        //tr.Translate(moveDir.normalized * Time.deltaTime * moveSpeed, Space.Self);
+    // 키 입력을 판별하는 코루틴
+    IEnumerator KeyDownCheckCoroutine()
+    {
+        while (true)
+        {
+            if (CharacterControl == true)
+            {
+                h = Input.GetAxis("Horizontal");
+                v = Input.GetAxis("Vertical");
+            }
 
-        //// Vector3.up 축을 기준으로 rotSpeed만큼의 속도로 회전
-        ////tr.Rotate(Vector3.up * Time.deltaTime * rotationSpeed * Input.GetAxis("Mouse X"));
-        //if (Input.GetKeyDown(KeyCode.S))
-        //{
-        //    banjun = true;
-        //    tr.Rotate(1.0f, 180.0f, 1.0f, 0);
-        //    //banjun = false;
-        //}
-        if (Input.GetKey(KeyCode.W))
-        {
-            tr.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
-            animator.SetBool("FrontKeyDown", true);
-            animator.SetBool("BackKeyDown", false);
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            tr.Translate(Vector3.back * moveSpeed * Time.deltaTime);
-            animator.SetBool("BackKeyDown", true);
-            animator.SetBool("FrontKeyDown", false);
-        }
-        else
-        {
-            animator.SetBool("FrontKeyDown", false);
-            animator.SetBool("BackKeyDown", false);
-        }
+            // 마우스 왼쪽 클릭시 첫번째 공격 애니메이션, 함수, 코루틴 실행
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                playerState = PlayerState.attackUpDown;
+                CharacterControlLimit();
+                animator.SetInteger("AttackNumber", 1);
+                StartCoroutine("AnimExitCheckCoroutine");
+            }
+            // 마우스 오른쪽 클릭시 첫번째 공격 애니메이션, 함수, 코루틴 실행
+            else if (Input.GetKeyDown(KeyCode.Mouse1))
+            {
+                playerState = PlayerState.attackDownUp;
+                CharacterControlLimit();
+                animator.SetInteger("AttackNumber", 2);
+                StartCoroutine("AnimExitCheckCoroutine");
+            }
+            // F키 누를 때 세번째 공격 애니메이션, 함수, 코루틴 실행
+            else if (Input.GetKeyDown(KeyCode.F))
+            {
+                playerState = PlayerState.attackHorizontal;
+                CharacterControlLimit();
+                animator.SetInteger("AttackNumber", 3);
+                StartCoroutine("AnimExitCheckCoroutine");
+            }
 
-        if (Input.GetKey(KeyCode.A))
-            tr.transform.Rotate(Vector3.up, -rotationSpeed * Time.deltaTime);
-        if (Input.GetKey(KeyCode.D))
-            tr.transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
-        
-        if(Input.GetKey(KeyCode.Mouse0))
-        {
-            animator.SetInteger("AttackNumber", 1);
+            // Space키 누를 때 구르기 애니메이션 및 코루틴 실행
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                playerState = PlayerState.escape;
+                animator.SetBool("SpaceCheck", true);
+                StartCoroutine("AnimExitCheckCoroutine");
+            }
+
+            //블랜드 트리에서 v값과 h 값을 계산해서 애니메이션 실행된다.
+            animator.SetFloat("Vertical", v);
+            animator.SetFloat("Horizontal", h);
+            yield return null;
         }
-        else
+    }
+
+    // 애니메이션 종료 체크 코루틴
+    IEnumerator AnimExitCheckCoroutine()
+    {
+        if (playerState == PlayerState.attackUpDown)
         {
+            yield return new WaitForSeconds(1.1f);
             animator.SetInteger("AttackNumber", 0);
+            CharacterControl = true;
         }
+        else if(playerState == PlayerState.attackDownUp)
+        {
+            yield return new WaitForSeconds(1.3f);
+            animator.SetInteger("AttackNumber", 0);
+            CharacterControl = true;
+        }
+        else if(playerState == PlayerState.attackHorizontal)
+        {
+            yield return new WaitForSeconds(1.5f);
+            animator.SetInteger("AttackNumber", 0);
+            CharacterControl = true;
+        }
+        else if(playerState == PlayerState.escape)
+        {
+            yield return new WaitForSeconds(0.3f);
+            animator.SetBool("SpaceCheck", false);
+        }
+        //StopCoroutine(AttackAnimExitCheckCoroutine());
+        playerState = PlayerState.idle;
+    }
 
+    // 캐릭터 이동 함수
+    void PlayerMove(float h, float v)
+    {
+        // 위치를 설정해주고 계산한다.
+        movement.Set(h, 0, v);
+        movement = movement.normalized * Time.deltaTime * moveSpeed;
 
-        // 블랜드 트리에서 v값과 h 값을 계산해서 애니메이션 실행된다.
-        //animator.SetFloat("Vertical", v);
-        //animator.SetFloat("Horizontal", h);
+        // rigidbody를 이용해 이동시킴
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
+    // 캐릭터 회전 함수
+    void PlayerRotation()
+    {
+        // 회전후 움직임이 없을 때 정면을 바라보는 행동을 제한하기 위함
+        if (h == 0 && v == 0)
+        {
+            return;
+        }
+            Quaternion newRotation = Quaternion.LookRotation(movement);    
+            playerRigidbody.rotation = Quaternion.Slerp(playerRigidbody.rotation, newRotation, rotationSpeed * Time.deltaTime);     
+    }
+
+    // 캐릭터가 공격할 때 이동의 제한을 위한 함수
+    void CharacterControlLimit()
+    {
+        if (CharacterControl == true)
+        {
+            CharacterControl = false;
+            h = 0;
+            v = 0;
+        }
     }
 }
