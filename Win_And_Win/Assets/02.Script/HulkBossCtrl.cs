d39a51197e833fb91de8be47f187adc900727587
DESKTOP-MS94ACF\shtke <shtkeh77@gmail.com>
2019-01-09 02:25:37 +0900

    컨셉 교체
    - 지금의 개발형태로 보면 블래이드앤소울의 레이드느낌이 더 괜찮은거 같다.
    - 다크소울, 블러드본 같은 게임을 기획 - > 교체
    - 결론: 블래이드앤소울의 레이드 형식의 컨셉으로 바꿔보자
    - 코드를 다시 작성하자

--- Win_And_Win/Assets/02.Script/HulkBossCtrl.cs
+++ Win_And_Win/Assets/02.Script/HulkBossCtrl.cs
@@ -17,9 +17,9 @@ public class HulkBossCtrl : MonoBehaviour
     private Animator animator;
 
     // 추적 사정거리
-    public float traceDist = 200.0f;
+    public float traceDist = 30.0f;
     // 공격 사정거리
-    public float attackDist = 6.0f;
+    public float attackDistPowerHook = 15.0f;
     // 네비메쉬를 사용하기 위함
     private NavMeshAgent nvAgent;
     //// 좀비 공격력
@@ -27,8 +27,7 @@ public class HulkBossCtrl : MonoBehaviour
 
     // 좀비의 사망 여부
     private bool isDie = false;
-    // 좀비 이동 제한을 위한 변수
-    public bool zombieMoveLimit = false;
+    private float realdist;
 
     //// 혈흔 효과 프리팹
     //public GameObject bloodEffect;
@@ -54,9 +53,8 @@ public class HulkBossCtrl : MonoBehaviour
 
         // 일정한 간격으로 좀비의 행동 상태를 체크하는 코루틴 함수 실행
         StartCoroutine(this.CheckZombieState());
-
-        //// 좀비의 상태에 따라 동작하는 루틴을 실행하는 코루틴 함수 실행
-        // StartCoroutine(this.ZombieAction());
+        // 좀비의 상태에 따라 동작하는 루틴을 실행하는 코루틴 함수 실행
+        StartCoroutine(this.ZombieAction());
     }
 
     // 좀비의 상태를 체크하는 코루틴
@@ -64,89 +62,62 @@ public class HulkBossCtrl : MonoBehaviour
     {
         while (!isDie)
         {
-            yield return new WaitForSeconds(0.3f);
-
-            // 좀비와 플레이어의 거리 측정
-            float dist = Vector3.Distance(playerTransform.position, hulkTransform.position);
-            if (dist <= attackDist) // 공격거리 범위 이내로 들어왔는지 확인
+            yield return new WaitForSeconds(0.2f);
+            //yield return null;
+            realdist = Vector3.Distance(playerTransform.position, hulkTransform.position);
+            if (realdist <= attackDistPowerHook)
             {
                 hulkState = HulkState.attack;
-                nvAgent.isStopped = true;
-                zombieMoveLimit = true;
-                animator.SetBool("IsTrace", false);
-                animator.SetInteger("AttackNumber", 1);
-                yield return new WaitForSeconds(1.5f);
-                zombieMoveLimit = false;
-
             }
-            else if (dist <= traceDist && zombieMoveLimit == false) // 추적거리 범위 이내로 들어왔는지 확인
-            {
-                hulkState = HulkState.walk;
-                // 추적 대상의 위치를 넘겨줌
-                nvAgent.destination = playerTransform.position;
-                // 추적을 재시작
-
-                //yield return new WaitForSeconds(0.9f);
-                //zombieMoveLimit = false;
-                //nvAgent.isStopped = false;
-
-                //nvAgent.isStopped = false;
-                animator.SetBool("IsTrace", true);
-                animator.SetInteger("AttackNumber", 0);
-            }
-            else if (dist > traceDist)
+            //else if (realdist <= traceDist) // 추적거리 범위 이내로 들어왔는지 확인
+            //{
+            //    hulkState = HulkState.walk;
+            //}
+            else if (realdist > traceDist)
             {
                 hulkState = HulkState.idle;
-                nvAgent.isStopped = true;
-                animator.SetBool("IsTrace", false);
-                animator.SetInteger("AttackNumber", 0);
             }
 
         }
     }
 
     // 좀비의 애니메이션을 설정하는 코루틴
-    //public IEnumerator ZombieAction()
-    //{
-    //    while (!isDie)
-    //    {
-    //        switch (hulkState)
-    //        {
-    //            case HulkState.idle:
-    //                nvAgent.isStopped = true;
-    //                animator.SetBool("IsTrace", false);
-    //                animator.SetInteger("AttackNumber", 0);
-    //                //animator.SetBool("IsAttack", false);
-    //                break;
+    public IEnumerator ZombieAction()
+    {
+        while (!isDie)
+        {
+            switch (hulkState)
+            {
+                case HulkState.idle:
+                    hulkTransform.LookAt(playerTransform);
+                    //nvAgent.isStopped = true;
+                    animator.SetBool("IsTrace", false);
+                    animator.SetInteger("AttackNumber", 0);
+                    break;
+                // 추적 상태
 
-    //            // 추적 상태
-    //            case HulkState.walk:
-    //                // 추적 대상의 위치를 넘겨줌
-    //                nvAgent.destination = playerTransform.position;
-    //                // 추적을 재시작
-    //                if(zombieMoveLimit == true)
-    //                {
-    //                    yield return new WaitForSeconds(0.9f);
-    //                    zombieMoveLimit = false;
-    //                    nvAgent.isStopped = false;
-    //                }
-    //                //nvAgent.isStopped = false;
-    //                animator.SetBool("IsTrace", true);
-    //                animator.SetInteger("AttackNumber", 0);
-    //                break;
+                //case HulkState.walk:
+                //    // 추적 대상의 위치를 넘겨줌
+                //    hulkTransform.LookAt(playerTransform);
+                //    //nvAgent.destination = playerTransform.position;
+                //    // 추적을 재시작                    
+                //    //nvAgent.isStopped = false;
+                //    animator.SetBool("IsTrace", true);
+                //    animator.SetInteger("AttackNumber", 0);                
+                //    break;
 
-    //            case HulkState.attack:
-    //                nvAgent.isStopped = true;
-    //                zombieMoveLimit = true;
-    //                animator.SetBool("IsTrace", false);
-    //                animator.SetInteger("AttackNumber", 1);
-    //                //yield return new WaitForSeconds(0.8f);
-    //                //zombieMoveLimit = false;
-    //                break;
-    //        }
-    //        yield return null;
-    //    }
-    //}
+                case HulkState.attack:                 
+                    //nvAgent.isStopped = true;
+                    animator.SetBool("IsTrace", false);
+                    animator.SetInteger("AttackNumber", 1);
+                    yield return new WaitForSeconds(1.0f);
+                    hulkTransform.LookAt(playerTransform);
+                    break;
+           
+            }
+            yield return null;
+        }
+    }
 
     //void ZombieMoveLimit()
     //{
