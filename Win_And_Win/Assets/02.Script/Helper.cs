771e0fe5550e145c8ce24e015328becb1fea062f
DESKTOP-MS94ACF\shtke <shtkeh77@gmail.com>
2019-01-09 19:51:16 +0900

    많은 애니메이션을 작업을 처리할 수 있게 분할 처리 - 애니메이션 체크 및 상태 처리를 할 수 있는 스크립트 추가 (Helper)

---
+++ Win_And_Win/Assets/02.Script/Helper.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HSS
+{
+    public class Helper : MonoBehaviour
+    {
+        [Range(-1, 1)]
+        public float vertical;
+        [Range(-1, 1)]
+        public float horizontal;
+
+        public string[] attacks;
+        public bool playAnim = true;
+        public bool enableRootMotion;
+        public bool drinking;
+        public bool interacting;
+        public bool lockOn;
+        Animator animator;
+
+
+        // Use this for initialization
+        void Start()
+        {
+            //animator = this.transform.GetChild(0).GetComponent<Animator>();
+            animator = GetComponent<Animator>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            enableRootMotion = !animator.GetBool("canMove");
+            animator.applyRootMotion = enableRootMotion;
+
+            interacting = animator.GetBool("interacting");
+
+            if (lockOn==false)
+            {
+                horizontal = 0;
+                vertical = Mathf.Clamp01(vertical);
+            }
+
+            animator.SetBool("lockOn", lockOn);
+
+            if (enableRootMotion)
+                return;
+
+            if (drinking)
+            {             
+                animator.Play("Drinking");
+                drinking = false;
+            }
+
+            if(interacting)
+            {
+                playAnim = false;
+                vertical = Mathf.Clamp(vertical, 0, 0.5f);
+            }
+
+            if (playAnim)
+            {
+                string targetAnim;
+                int r = Random.Range(0, attacks.Length);
+                targetAnim = attacks[r];
+
+                if (vertical > 0.5f)
+                    targetAnim = "attack_3";
+
+                vertical = 0;
+
+                animator.CrossFade(targetAnim, 0.2f);
+                //animator.SetBool("canMove", false);
+                //enableRootMotion = true;
+                playAnim = false;
+            }
+            animator.SetFloat("Vertical",vertical);
+            animator.SetFloat("Horizontal", horizontal);
+        }
+    }
+}
