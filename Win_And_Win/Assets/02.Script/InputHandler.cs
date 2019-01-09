e5283f65f0e7072838d689b3ab0386984f8334ae
DESKTOP-MS94ACF\shtke <shtkeh77@gmail.com>
2019-01-09 22:43:24 +0900

    3인칭 컨트롤러 구현 - 카메라 관리 - 캐릭터 입력 관리 - 캐릭터 상태 관리

---
+++ Win_And_Win/Assets/02.Script/InputHandler.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HSS
+{
+    public class InputHandler : MonoBehaviour
+    {
+        float vertical;
+        float horizontal;
+        bool runInput;
+
+        StateManager states;
+        CameraManager camManager;
+
+        float delta;
+
+        // Use this for initialization
+        void Start()
+        {
+            states = GetComponent<StateManager>();
+            states.Init();
+
+            camManager = CameraManager.singleton;
+            camManager.Init(this.transform);
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            delta = Time.fixedDeltaTime;
+            GetInput();
+            UpdateStates();
+
+            states.FixedTick(delta);
+            camManager.Tick(delta);
+        }
+
+        void Update()
+        {
+            delta = Time.deltaTime;
+            states.Tick(delta);
+
+        }
+
+        void GetInput()
+        {
+            vertical = Input.GetAxis("Vertical");
+            horizontal = Input.GetAxis("Horizontal");
+            runInput = Input.GetButton("RunInput");
+        }
+
+        void UpdateStates()
+        {
+            states.horizontal = horizontal;
+            states.vertical = vertical;
+
+            Vector3 v = states.vertical * camManager.transform.forward;
+            Vector3 h = states.horizontal * camManager.transform.right;
+            states.moveDir = (v + h).normalized;
+            float m = Mathf.Abs(horizontal) + Mathf.Abs(vertical);
+            states.moveAmount = Mathf.Clamp01(m);
+
+            if (runInput)
+            {
+                states.run = (states.moveAmount > 0);                
+            }
+            else
+            {
+                states.run = false;
+            }
+        }
+    }
+
+
+}
---
+++ Win_And_Win/Assets/02.Script/StateManager.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HSS
+{
+    public class StateManager : MonoBehaviour
+    {
+        [Header("Init")]
+        public GameObject activeModel;
+
+        [Header("Inputs")]
+        public float vertical;
+        public float horizontal;
+        public float moveAmount;
+        public Vector3 moveDir;
+
+        [Header("Stats")]
+        public float moveSpeed = 2;
+        public float runSpeed = 3.5f;
+        public float rotateSpeed = 5;
+        public float toGround = 0.5f;
+         
+        [Header("States")]
+        public bool run;
+        public bool onGround;
+        public bool lockOn;
+
+
+        [HideInInspector]
+        public Animator animator;
+        [HideInInspector]
+        public Rigidbody rigid;
+
+        [HideInInspector]
+        public float delta;
+        [HideInInspector]
+        public LayerMask ignoreLayers;
+
+        public void Init()
+        {
+            SetUpAnimator();
+            rigid = GetComponent<Rigidbody>();
+            rigid.angularDrag = 999;
+            rigid.drag = 4;
+            rigid.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+
+            gameObject.layer = 8;
+            ignoreLayers = ~(1 << 9);
+
+            animator.SetBool("onGround", true);
+        }
+
+        void SetUpAnimator()
+        {
+            if (activeModel == null)
+            {
+                animator = GetComponentInChildren<Animator>();
+                if (animator == null)
+                {
+                    Debug.Log("애니메이션을 찾을 수 없다.");
+                }
+                else
+                {
+                    activeModel = animator.gameObject;
+                }
+            }
+            if (animator == null)
+                animator = activeModel.GetComponent<Animator>();
+
+            animator.applyRootMotion = false;
+        }
+
+        public void FixedTick(float d)
+        {
+            delta = d;
+
+            rigid.drag = (moveAmount > 0 || onGround == false) ? 0 : 4;      
+         
+            float targetSpeed = moveSpeed;
+
+            if (run)
+            {
+                targetSpeed = runSpeed;
+                lockOn = false;
+            }
+            
+            if(onGround)
+                rigid.velocity = moveDir * (targetSpeed * moveAmount);
+
+            if (!lockOn)
+            {
+                Vector3 targetDir = moveDir;
+                targetDir.y = 0;
+                if (targetDir == Vector3.zero)
+                {
+                    targetDir = transform.forward;
+                }
+
+                Quaternion tr = Quaternion.LookRotation(targetDir);
+                Quaternion targetRotation = Quaternion.Slerp(transform.rotation, tr, delta * moveAmount * rotateSpeed);
+                transform.rotation = targetRotation;
+
+            }
+            HandleMovementAnmations();
+        }
+
+        public void Tick(float d)
+        {
+            delta = d;
+            onGround = OnGround();
+            animator.SetBool("onGround", onGround);
+        }
+
+        void HandleMovementAnmations()
+        {
+            animator.SetBool("run", run);
+            animator.SetFloat("Vertical", moveAmount,0.4f,delta);
+        }
+
+        public bool OnGround()
+        {
+            bool r = false;
+
+            Vector3 origin = transform.position + (Vector3.up * toGround);
+            Vector3 dir = -Vector3.up;
+            
+            // 거리
+            float dis = toGround + 0.3f;
+
+            RaycastHit hit;
+            if(Physics.Raycast(origin,dir,out hit, dis, ignoreLayers))
+            {
+                r = true;
+                Vector3 targetPosition = hit.point;
+                transform.position = targetPosition;
+               
+            }
+            return r;
+        }
+    }
+}
